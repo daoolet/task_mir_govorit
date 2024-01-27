8d19104e11ea0c832aee99ee3f2f2fb62f0a073a
daoolet-honeywell <daulet.khabidullin@honeywell.com>
2024-01-28 00:39:01 +0500

    add: drf, swagger

--- app_cook/urls.py
+++ app_cook/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.api_index, name="index"),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
+
+
--- project_cook/settings.py
+++ project_cook/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_cook',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'MirGovorit Tesk Task'
+}
