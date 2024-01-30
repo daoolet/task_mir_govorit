4963c8d44e6d7ed8a7630fb4180a4ff929a59d1e
daoolet-honeywell <daulet.khabidullin@honeywell.com>
2024-01-30 10:11:47 +0500

    change: shifted to Class-based view

--- app_cook/urls.py
+++ app_cook/urls.py
@@ -6,11 +6,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("products/", views.get_products, name="get_products"),
-    path("recipes/", views.get_recipes, name="get_recipes"),
-
-    path("products/add/", views.post_product, name="post_product"),
-    path("recipes/add/", views.post_recipes, name="post_recipes"),
+    path("products/", views.ProductsView.as_view(), name="products_view"),
+    path("recipes/", views.RecipesView.as_view(), name="recipes_view"),
 
     path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
--- app_cook/views.py
+++ app_cook/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render, get_object_or_404
 
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from drf_spectacular.utils import extend_schema
 
 from .models import Product, Recipe, RecipeProduct
 from .serializers import ProductSerializer, RecipeSerializer
@@ -15,55 +19,54 @@ def index(request):
     return Response("This is GET", status=status.HTTP_200_OK)
 
 
-@api_view(["GET"])
-def get_products(request):
-    products = utils.get_all_products()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data, status=status.HTTP_418_IM_A_TEAPOT)
+class ProductsView(GenericAPIView):
+    def get(self, request):
+        products = utils.get_all_products()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_418_IM_A_TEAPOT)
+    
+    @extend_schema(request=ProductSerializer)
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+            product_name = serializer.validated_data["name"]
+
+            if utils.product_exists(name=product_name):
+                return Response({"detail": "Already exists"}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                product = serializer.save()
+                return Response({
+                    "detail": "Successful",
+                    "product_id": product.id,
+                    }, status=status.HTTP_200_OK)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
 
 
-@api_view(["GET"])
-def get_recipes(request):
-    recipes = utils.get_all_recipes()
-    serializer = RecipeSerializer(recipes, many=True)
-    return Response(serializer.data, status=status.HTTP_418_IM_A_TEAPOT)
+class RecipesView(GenericAPIView):
+    def get(self, request):
+        recipes = utils.get_all_recipes()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data, status=status.HTTP_418_IM_A_TEAPOT)
 
+    @extend_schema(request=RecipeSerializer)
+    def post(self, request):
+        serializer = RecipeSerializer(data=request.data)
 
-@api_view(["POST"])
-def post_product(request):
-    serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            recipe_name = serializer.validated_data["name"]
 
-    if serializer.is_valid():
-        product_name = serializer.validated_data["name"]
-
-        if utils.product_exists(name=product_name):
-            return Response({"detail": "Already exists"}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            product = serializer.save()
-            return Response({
-                "detail": "Successful",
-                "product_id": product.id,
-                }, status=status.HTTP_200_OK)
-        
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["POST"])
-def post_recipes(request):
-    serializer = RecipeSerializer(data=request.data)
-
-    if serializer.is_valid():
-        recipe_name = serializer.validated_data["name"]
-
-        if utils.recipe_exists(name=recipe_name):
-            return Response({"detail": "Already exists"}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            recipe = serializer.save()
-            return Response({
-                "detail": "Successful",
-                "recipe_id": recipe.id,
-                }, status=status.HTTP_200_OK)
-        
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+            if utils.recipe_exists(name=recipe_name):
+                return Response({"detail": "Already exists"}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                recipe = serializer.save()
+                return Response({
+                    "detail": "Successful",
+                    "recipe_id": recipe.id,
+                    }, status=status.HTTP_200_OK)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
