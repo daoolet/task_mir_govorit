9421ae400559a4d30ef24ffb400ea0e03a264c93
Daoolet <dauletkhabidullin@gmail.com>
2024-02-24 21:56:36 +0600

    change: admin file

--- app_cook/admin.py
+++ app_cook/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Product, Recipe
+from .models import Product, Recipe, RecipeProduct
 
 # Register your models here.
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "amount_of_prep")
 
-admin.site.register(Product)
+@admin.register(RecipeProduct)
+class RecipeProductAdmin(admin.ModelAdmin):
+    list_display = ("recipe", "product", "weight")
+    list_filter = ("recipe", "product")
+
+
 admin.site.register(Recipe)
